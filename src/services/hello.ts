ab0a3b5bc5fb758440e5a87d628e517dd3c44fe8
hazeliscoding <duck_soop@outlook.com>
2024-04-21 13:05:21 -0500

    feat: 🎸 aws sdk

--- src/services/hello.ts
+++ src/services/hello.ts
@@ -5,11 +5,17 @@ import {
 } from 'aws-lambda';
 
 import { v4 } from 'uuid';
+import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3';
+
+const s3Client = new S3Client({})
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
+  const command = new ListBucketsCommand({});
+  const listBucketsResult = (await s3Client.send(command)).Buckets;
+
   const response: APIGatewayProxyResult = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda! This is the id: ' + v4()),
+    body: JSON.stringify(`Hello from Lambda! Here is the id: ${v4()} - Buckets: ${JSON.stringify(listBucketsResult)}`),
   };
 
   console.log(event);
