e3bf9f8f27b6ef8aee2bccfee095e1c0859e1955
hazeliscoding <duck_soop@outlook.com>
2024-04-21 02:32:36 -0500

    feat: 🎸 aws dynamodb with cdk

--- src/infra/services/hello.js
+++ src/infra/services/hello.js
@@ -1,8 +1,6 @@
 exports.main = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello from lambda!',
-    }),
+    body: JSON.stringify(`Hello! I will read from ${process.env.TABLE_NAME}`),
   };
 };
