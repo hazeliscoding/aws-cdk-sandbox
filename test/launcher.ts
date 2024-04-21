a26c460dba7969b0fb2ee9362f2b9eb4e8f7ad43
hazeliscoding <duck_soop@outlook.com>
2024-04-21 14:41:49 -0500

    feat: 🎸 testing queries

--- test/launcher.ts
+++ test/launcher.ts
@@ -1,3 +1,12 @@
-import { handler } from '../src/services/hello';
+import { handler } from '../src/services/spaces/handler';
 
-handler({} as any, {} as any);
+process.env.AWS_REGION = 'us-east-1';
+process.env.TABLE_NAME = 'SpaceTable-12b56d6b82f9';
+
+handler(
+  {
+    httpMethod: 'POST',
+    body: JSON.stringify({ location: 'Stockholm' }),
+  } as any,
+  {} as any
+);
