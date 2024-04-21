09dab5c2ba4e74191a56e9fa013ff4534cc81d0e
hazeliscoding <duck_soop@outlook.com>
2024-04-21 14:18:36 -0500

    feat: 🎸 put item

--- src/services/spaces/handler.ts
+++ src/services/spaces/handler.ts
@@ -1,8 +1,12 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   Context,
 } from 'aws-lambda';
+import { postSpaces } from './PostSpaces';
+
+const dbbClient = new DynamoDBClient({});
 
 async function handler(
   event: APIGatewayProxyEvent,
@@ -10,15 +14,23 @@ async function handler(
 ): Promise<APIGatewayProxyResult> {
   let message: string;
 
-  switch (event.httpMethod) {
-    case 'GET':
-      message = 'Hello from GET!';
-      break;
-    case 'POST':
-      message = 'Hello from POST!';
-      break;
-    default:
-      break;
+  try {
+    switch (event.httpMethod) {
+      case 'GET':
+        message = 'Hello from GET!';
+        break;
+      case 'POST':
+        const response = postSpaces(event, dbbClient);
+        return response;
+      default:
+        break;
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    }
   }
 
   const response: APIGatewayProxyResult = {
