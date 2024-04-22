4f46b2a5f0229235e37c88378f9b7607c7658502
hazeliscoding <duck_soop@outlook.com>
2024-04-22 18:37:01 -0500

    feat: 🎸 identity pools

--- src/infra/stacks/AuthStack.ts
+++ src/infra/stacks/AuthStack.ts
@@ -1,15 +1,16 @@
 import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import {
+  CfnIdentityPool,
   CfnUserPoolGroup,
   UserPool,
   UserPoolClient,
 } from 'aws-cdk-lib/aws-cognito';
-import { CfnUserGroup } from 'aws-cdk-lib/aws-elasticache';
 import { Construct } from 'constructs';
 
 export class AuthStack extends Stack {
   public userPool: UserPool;
   private userPoolClient: UserPoolClient;
+  private identityPool: CfnIdentityPool;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -17,6 +18,7 @@ export class AuthStack extends Stack {
     this.createUserPool();
     this.createUserPoolClient();
     this.createAdminsGroup();
+    this.createIdentityPool();
   }
 
   private createUserPool() {
@@ -52,6 +54,22 @@ export class AuthStack extends Stack {
     new CfnUserPoolGroup(this, 'SpaceAdmins', {
       userPoolId: this.userPool.userPoolId,
       groupName: 'admins',
+    });
+  }
+
+  private createIdentityPool() {
+    this.identityPool = new CfnIdentityPool(this, 'SpaceIdentityPool', {
+      allowUnauthenticatedIdentities: true,
+      cognitoIdentityProviders: [
+        {
+          clientId: this.userPoolClient.userPoolClientId,
+          providerName: this.userPool.userPoolProviderName,
+        },
+      ],
+    });
+
+    new CfnOutput(this, 'SpaceIdentityPoolId', {
+      value: this.identityPool.ref,
     });
   }
 }
